d98207b15b495af9b78bbbb629084967ca35c1e5
Rundll86 <17784453279@163.com>
2024-11-16 19:58:53 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    title: 'FS-Context',
+    description: 'A simple TypeScript context library for ScratchExtension',
+    themeConfig: {
+        displayAllHeaders: true,
+        sidebar: "auto",
+        smoothScroll: true
+    }
+}
